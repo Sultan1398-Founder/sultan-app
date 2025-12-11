45ceff97c35a557ac429622f2b9a0b3ebc1c6529
Osama Husam <osama.jawanh@gmail.com>
2025-12-11 10:24:28 +0200

    fix color

--- components/dashboard-preview.tsx
+++ components/dashboard-preview.tsx
@@ -6,7 +6,7 @@ export function DashboardPreview() {
 
   return (
     <div className="w-[calc(100vw-32px)] md:w-[1160px]">
-      <div className="bg-[hsl(25,100%,65%)]/50 rounded-2xl p-2 shadow-2xl">
+      <div className="bg-[hsl(210,100%,65%)]/50 rounded-2xl p-2 shadow-2xl">
         <video
           src="/vidoe.webm"
           loop
