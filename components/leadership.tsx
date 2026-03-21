45356a4b7414727629ba11425688e936c04f1675
Dr.Sultan Alsajjan <s.alsajjan@live.com>
2026-03-21 15:59:00 +0300

    Update image link in leadership.tsx

--- components/leadership.tsx
+++ components/leadership.tsx
@@ -24,7 +24,7 @@ export function LeadershipSection() {
         },
         {
           "name": isRtl ?  "م. أحمد حسين كتاب" : "Eng. Ahmed Hussein Kattab",
-          "img":"/1000088406.png",
+          "img":"ahmed.jpeg",
           "role": isRtl ? "كبير المبرمجين" : "Chief Programmer"
         },
         {
